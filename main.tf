c2a794973154960d1be156445460027977da3cbf
aditya642 <aditya642@users.noreply.github.com>
2025-08-25 14:00:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source              = "./module/resource_group"
+  source              = "/module/resource_group"
   resource_group_name = "rg_example1"
   location            = "central india"
 }
